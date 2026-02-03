5e9412e41e0a8ca8f994acdbb1479324f029fc16
Efrata Habte <efratahabte1213@gmail.com>
2026-02-03 15:45:57 +0300

    Update Palindrom Number (LC).py

--- Palindrom Number (LC).py
+++ Palindrom Number (LC).py
@@ -2,3 +2,19 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         x=str(x)
         return x==x[::-1]
+
+# optimized solution
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x<0:
+            return False
+        
+        reversed_num=0
+        n=x
+
+        while n>0:
+            digit=n%10
+            reversed_num = reversed_num*10 +digit
+            n//=10
+        
+        return x==reversed_num
