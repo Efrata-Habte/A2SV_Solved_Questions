9e9a4c7ac5df89fdba9800bce0132c848c6259f2
Efrata Habte <efratahabte1213@gmail.com>
2026-03-03 10:27:09 +0300

    0985-sum-of-even-numbers-after-queries Medium 24 MB (93.79%) 4 ms (96.66%) 03-03-2026

---
+++ LeetCode/Medium/0985-sum-of-even-numbers-after-queries/0985-sum-of-even-numbers-after-queries-03-03-2026-10-27-10.py
@@ -0,0 +1,23 @@
+class Solution:
+    def sumEvenAfterQueries(self, nums: List[int], queries: List[List[int]]) -> List[int]:
+        n = len(nums)
+        answer = []
+        even_sum = 0
+
+        for i in nums:
+            if i % 2 == 0:
+                even_sum += i
+
+        for val, idx in queries:
+            if nums[idx] % 2 == 0:
+                even_sum -= nums[idx]
+
+            nums[idx] += val
+
+            if nums[idx] % 2 == 0:
+                even_sum += nums[idx]
+            
+            answer.append(even_sum)
+        
+        return answer
+
