46866ca07c29c69cbf82fdc8dbe37f6e9c2d21bb
Efrata Habte <efratahabte1213@gmail.com>
2026-03-03 14:54:25 +0300

    0380-insert-delete-getrandom-o1 Medium 56.9 MB (60.4%) 107 ms (62.12%) 03-03-2026

---
+++ LeetCode/Medium/0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1-03-03-2026-14-54-25.py
@@ -0,0 +1,32 @@
+import random
+
+class RandomizedSet:
+
+    def __init__(self):
+        self.container = {} 
+        self.nums = []        
+
+    def insert(self, val: int) -> bool:
+        if val in self.container:
+            return False
+
+        self.container[val] = len(self.nums)
+        self.nums.append(val)
+        return True
+
+    def remove(self, val: int) -> bool:
+        if val not in self.container:
+            return False
+
+        idx = self.container[val]
+        last = self.nums[-1]
+
+        self.nums[idx] = last
+        self.container[last] = idx
+
+        self.nums.pop()
+        del self.container[val]
+        return True
+
+    def getRandom(self) -> int:
+        return random.choice(self.nums)
