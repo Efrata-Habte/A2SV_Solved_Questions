0cc875fff7e1ac96798555b5a5b40f8419d21222
Efrata Habte <efratahabte1213@gmail.com>
2026-03-04 16:41:53 +0300

    Add solution for Maximum Subarray

--- leetcode/0053-maximum-subarray/0053-maximum-subarray.py
+++ leetcode/0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,8 +1,11 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        max_ = nums[0]
 
-        for right in range(1,len(nums)):
-            if nums[right-1]>=0:
-                nums[right]+=nums[right-1]
+        for right in range(1, len(nums)):
+            if nums[right - 1] >= 0:
+                nums[right] += nums[right - 1]
 
-        return max(nums)
+            max_ = max(max_, nums[right])
+
+        return max_
