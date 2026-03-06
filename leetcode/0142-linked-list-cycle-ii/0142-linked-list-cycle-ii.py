34a56c53cce939f68f8ae1656aabdafc1f2a649a
Efrata Habte <efratahabte1213@gmail.com>
2026-03-06 17:13:14 +0300

    Add solution for Linked List Cycle II

---
+++ leetcode/0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        fast = head
+        slow = head
+        cycle = False
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+            if fast == slow:
+                cycle = True
+                break
+        
+        if cycle:
+            slow = head
+
+        while slow != fast and cycle:
+            slow = slow.next
+            fast = fast.next
+
+        return slow if cycle else None 
+        
