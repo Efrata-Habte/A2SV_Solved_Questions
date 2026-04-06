632929ff83827c6c7edc31146093ff9e11375289
Efrata Habte <efratahabte1213@gmail.com>
2026-04-06 22:16:43 +0300

    0832-flipping-an-image Easy 19.3 MB (71.54%) 0 ms (100%) 04-06-2026

---
+++ LeetCode/Easy/0832-flipping-an-image/0832-flipping-an-image-04-06-2026-19-16-42.py
@@ -0,0 +1,10 @@
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        n = len(image)
+
+        for i in range(n):
+            image[i] = image[i][::-1]
+            for j in range(n):
+                image[i][j] = 0 if image[i][j]==1 else 1
+        
+        return image
